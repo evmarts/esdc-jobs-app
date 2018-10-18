ebb21fc1ac76820314d3f681f8e5c84663f27eb6
Evan Martin <evan.martin@shaw.ca>
2018-10-17 21:04:04 -0700

    starting a new layout

--- src/component/App.js
+++ src/component/App.js
@@ -4,10 +4,58 @@ import FetchJobs from "./FetchJobs";
 import RangeSelect from "./RangeSelect";
 import RangeForm from "./RangeForm";
 import SearchTool from "./SearchTool";
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from "react-bootstrap";
 
 class App extends Component {
   state = {
-    response: ""
+    response: "",
+    isFormVisible: false,
+    isSearchVisible: false
+  };
+
+  handleFormSelect = () => {
+    this.setState({
+      isFormVisible: true,
+      isSearchVisible: false
+    });
+  };
+
+  handleSearchSelect = () => {
+    this.setState({
+      isSearchVisible: true,
+      isFormVisible: false
+    });
+  };
+
+  TopNav = () => {
+    return (
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand
+            onClick={() => {
+              this.setState({ isFormVisible: false, isSearchVisible: false });
+            }}
+          >
+            ESDC Occupation Search
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} onClick={this.handleFormSelect}>
+            Form
+          </NavItem>
+          <NavItem eventKey={2} onClick={this.handleSearchSelect}>
+            Search
+          </NavItem>
+          {/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          </NavDropdown> */}
+        </Nav>
+      </Navbar>
+    );
   };
 
   onSubmit = async fields => {
@@ -24,11 +72,29 @@ class App extends Component {
 
   render() {
     // add more information to the displayed results
+    if (this.state.isFormVisible) {
+      return (
+        <div>
+          <this.TopNav />
+          <div>Form here</div>
+        </div>
+      );
+    }
+    if (this.state.isSearchVisible) {
+      return (
+        <div>
+          <this.TopNav />
+          <div>Search here</div>
+        </div>
+      );
+    }
+
     return (
       <div>
+        <this.TopNav />
         {/* <SearchTool /> */}
-        <Form onSubmit={fields => this.onSubmit(fields)} />
-        <this.DisplayResult />
+        {/* <Form onSubmit={fields => this.onSubmit(fields)} /> */}
+        {/* <this.DisplayResult /> */}
       </div>
     );
   }
